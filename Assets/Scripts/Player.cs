0a75099461a5381493ffd19972ef7c20e7581eec
DEMORGNY Gilbert <gdemorgny@dreamtogo.fr>
2023-02-14 16:33:23 +0100

    finish base

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,7 +13,15 @@ public class Player : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+<<<<<<< Updated upstream
         _scoreText.text = "Score : " + ScoreValue;
+=======
+        if(SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            ScoreValue = PlayerPrefs.GetInt("ScoreValue");
+        }
+        _scoreText.text = "Score : "+ScoreValue;
+>>>>>>> Stashed changes
     }
 
     void Update()
@@ -39,12 +47,32 @@ public class Player : MonoBehaviour
         {
             Destroy(collision.gameObject);
             UpdateScore();
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         }
     }
 
     private void UpdateScore()
     {
+        Instantiate(_wallPrefab, _scenario.FirstWalls[ScoreValue%_scenario.FirstWalls.Length].position,Quaternion.Euler(_scenario.FirstWalls[ScoreValue % _scenario.FirstWalls.Length].orientation));
+
         ScoreValue++;
+<<<<<<< Updated upstream
         _scoreText.text = "Score : " + ScoreValue;
+=======
+        if (PlayerPrefs.HasKey("Score"))
+        {
+            PlayerPrefs.SetString("Score", "Score : " + ScoreValue.ToString());
+        }
+        _scoreText.text = PlayerPrefs.GetString("Score");
+        if(ScoreValue >= 8 && SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            PlayerPrefs.SetInt("ScoreValue", ScoreValue);
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+>>>>>>> Stashed changes
     }
 }
